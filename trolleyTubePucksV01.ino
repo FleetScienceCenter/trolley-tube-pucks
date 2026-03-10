bfe441f93a12e3ecc7eae9538a141ea0c1af03f9
FleetScienceCenter <exhibits@rhfleet.org>
2026-03-10 13:47:23 -0700

    Add files via upload

--- trolleyTubePucksV01.ino
+++ trolleyTubePucksV01.ino
@@ -1,11 +1,24 @@
 #include <FastLED.h>
 #include <Adafruit_VCNL4010.h>
+#include <SD.h>
+#include <Audio.h>
 
 #define DATA_PIN 41
 #define NUM_LEDS 30
 CRGB leds[NUM_LEDS];
 
 Adafruit_VCNL4010 vcnl;
+
+AudioPlaySdWav playWav1;
+AudioOutputI2S audioOutput;
+
+AudioConnection patchCord1(playWav1, 0, audioOutput, 0);
+AudioConnection patchCord2(playWav1, 1, audioOutput, 1);
+AudioControlSGTL5000 sgtl5000_1;
+
+#define SDCARD_CS_PIN    BUILTIN_SDCARD
+#define SDCARD_MOSI_PIN  11
+#define SDCARD_SCK_PIN   13
 
 float baseline;
 float proxVal;
@@ -24,11 +37,23 @@ void setup() {
   Serial.println("Found VCNL4010");
   baseline = vcnl.readProximity();
   FastLED.showColor(CRGB(255, 0, 0));
+
+  AudioMemory(8);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(0.5);
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    // stop here, but print a message repetitively
+    while (1) {
+      Serial.println("Unable to access the SD card");
+      delay(500);
+    }
+  }
 }
 
 void showHit() {
   gVal = 255;
   bVal = 255;
+  playWav1.play("F#4.WAV");
   FastLED.showColor(CRGB(255, gVal, bVal));
   delay(500);
   while(gVal > 0 && bVal > 0){
