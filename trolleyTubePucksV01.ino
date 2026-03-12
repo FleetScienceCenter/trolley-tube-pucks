9b4be69f3921ab9161ad4eeed6027abbc741506e
FleetScienceCenter <exhibits@rhfleet.org>
2026-03-12 14:34:45 -0700

    Add files via upload

--- trolleyTubePucksV01.ino
+++ trolleyTubePucksV01.ino
@@ -4,7 +4,7 @@
 #include <Audio.h>
 
 #define DATA_PIN 41
-#define NUM_LEDS 30
+#define NUM_LEDS 60
 CRGB leds[NUM_LEDS];
 
 Adafruit_VCNL4010 vcnl;
@@ -16,9 +16,12 @@ AudioConnection patchCord1(playWav1, 0, audioOutput, 0);
 AudioConnection patchCord2(playWav1, 1, audioOutput, 1);
 AudioControlSGTL5000 sgtl5000_1;
 
-#define SDCARD_CS_PIN    BUILTIN_SDCARD
-#define SDCARD_MOSI_PIN  11
-#define SDCARD_SCK_PIN   13
+#define SDCARD_CS_PIN 10
+#define SDCARD_MOSI_PIN 11
+#define SDCARD_SCK_PIN 13
+
+const int delayTime = 1000;
+unsigned long idleTime = 0;
 
 float baseline;
 float proxVal;
@@ -26,20 +29,23 @@ bool hit = false;
 int rVal = 255;
 int gVal = 255;
 int bVal = 255;
+const char* notes[] = {"F#4.WAV", "G#4.WAV", "A#4.WAV"};
+
 
 void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
-  if (!vcnl.begin()){
+  if (!vcnl.begin()) {
     Serial.println("Sensor not found");
-    while (1);
+    while (1)
+      ;
   }
   Serial.println("Found VCNL4010");
   baseline = vcnl.readProximity();
-  
+
   AudioMemory(8);
   sgtl5000_1.enable();
-  sgtl5000_1.volume(0.5);
+  sgtl5000_1.volume(1.0);
   if (!(SD.begin(SDCARD_CS_PIN))) {
     while (1) {
       Serial.println("Unable to access the SD card");
@@ -52,10 +58,10 @@ void setup() {
 void showHit() {
   gVal = 255;
   bVal = 255;
-  playWav1.play("F#4.WAV");
+  playWav1.play(notes[rand() % 3]);
   FastLED.showColor(CRGB(255, gVal, bVal));
   delay(500);
-  while(gVal > 0 && bVal > 0){
+  while (gVal > 0 && bVal > 0) {
     gVal--;
     bVal--;
     FastLED.showColor(CRGB(255, gVal, bVal));
@@ -67,9 +73,10 @@ void showHit() {
 
 void loop() {
   proxVal = vcnl.readProximity();
-  if (!hit && proxVal > (baseline + 600)){
+  if ((!hit && proxVal > (baseline + 600))) {
     hit = true;
     showHit();
+    idleTime = millis() + delayTime;
   }
   Serial.println(proxVal);
 }
